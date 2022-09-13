dcb8c54990f547b2f3b9691389d1a14fbf366f19
masangana <alexmasangana@gmail.com>
2022-09-14 00:11:57 +0300

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,21 @@
 class User < ApplicationRecord
     has_many :comments
     has_many :posts
-    has_many :likes 
+    has_many :likes
+    
+    def update_posts_counter
+        update(posts_counter: posts.count)
+    end
+
+    def last_post
+        posts.order(created_at: :desc).first
+    end
+
+    def recent_comments
+        comments.order(created_at: :desc).limit(5)
+    end
+
+    def recent_posts
+        posts.order(created_at: :desc).limit(5)
+    end
 end
