fa4e49b33c842e40b99705c70f952e9dadfae84c
masangana <alexmasangana@gmail.com>
2022-09-20 01:35:33 +0300

    Write test for users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  before(:example) { get '/users' }
+
   describe 'GET /index' do
     it 'returns http success' do
-      get '/users/index'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns correct template' do
+      expect(response).to render_template('index')
     end
   end
 
   describe 'GET /show' do
+    before(:example) { get '/users/1' }
+    
     it 'returns http success' do
-      get '/users/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns correct template' do
+      expect(response).to render_template('show')
     end
   end
 end
