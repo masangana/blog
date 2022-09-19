70c2c6474de7eda8bff80790751607ec07306c44
masangana <alexmasangana@gmail.com>
2022-09-19 20:10:47 +0300

    Create controller and view for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
+  
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+=======
+>>>>>>> parent of 52ebb3e (Add controllers for users)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   ressources :users, only: [:index, :show]
   ressources :posts, only: [:index, :show]
