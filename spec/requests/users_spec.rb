507d5a69fa75d39a7357208bf8743f83a23b65cf
masangana <alexmasangana@gmail.com>
2022-09-20 01:50:18 +0300

    Update users test
    Add a test to check the body structure

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,6 +11,10 @@ RSpec.describe 'Users', type: :request do
     it 'returns correct template' do
       expect(response).to render_template('index')
     end
+
+    it 'returns a list of users' do
+      expect(response.body).to include('Will list all users')
+    end
   end
 
   describe 'GET /show' do
@@ -22,6 +26,10 @@ RSpec.describe 'Users', type: :request do
 
     it 'returns correct template' do
       expect(response).to render_template('show')
+    end
+
+    it 'returns correct body' do
+      expect(response.body).to include('Will show all user details')
     end
   end
 end
