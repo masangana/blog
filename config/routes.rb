dbe692f68bfed050962a3430cb77b5bde394b9a2
masangana <alexmasangana@gmail.com>
2022-09-21 22:43:38 +0300

    Create resources for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  #get 'comments/new'
+  #get 'comments/create'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show] do
+      resources :comments, only: %i[new create]
+    end
   end
 end
