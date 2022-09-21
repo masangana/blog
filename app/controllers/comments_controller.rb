b98f58db3027a0d79ef72d94f89d50fb610ac695
masangana <alexmasangana@gmail.com>
2022-09-22 00:46:29 +0300

    Add comment success

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,28 @@
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.new
+    render :new
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.post = Post.find(params[:post_id])
+
+    respond_to do |format|
+      if @comment.save
+
+        redirect_to user_post_path(@comment.post.user, @comment.post)
+      else
+        render :new
+      end
+    end
+    @comment.update_comments_counter
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
