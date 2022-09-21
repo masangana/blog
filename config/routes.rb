a8b421d605811e2714500b2d5e9303d71b380e46
masangana <alexmasangana@gmail.com>
2022-09-22 01:51:47 +0300

    Add a new post success

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   #get 'users/:user_id/:post_id/new_comment', to: 'comments#new', as: 'comment_new'
   #get 'comments/create'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show] do
+    resources :posts, only: %i[index show new create] do
       resources :comments, only: %i[new create]
     end
   end
