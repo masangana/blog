b064e6d83f0e6238d31e679fd5e16d35613381cc
masangana <alexmasangana@gmail.com>
2022-09-22 20:15:49 +0300

    Like success added

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,11 @@
 class LikesController < ApplicationController
-  def new
-  end
 
   def create
+    @user_like = current_user
+    @post_like = Post.find(params[:id])
+    @like = Like.new(user: @user_like, post: @post_like)
+    @like.update_likes_counter if @like.save
+    redirect_back_or_to user_post_path(@post_like.user.id, @post_like.id)
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'likes/new'
-  get 'likes/create'
+  #get 'likes/new'
+  #get 'likes/create'
   #get 'comments/new'
   #get 'users/:user_id/:post_id/new_comment', to: 'comments#new', as: 'comment_new'
   #get 'comments/create'
+  post 'users/:user_id/posts/:id/create_like', to: 'likes#create', as: 'like_create'
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: %i[new create]
