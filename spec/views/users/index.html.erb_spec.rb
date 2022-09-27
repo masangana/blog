38e459a5ede8da619c1d8ab95cb9362c0f03657a
masangana <alexmasangana@gmail.com>
2022-09-27 18:07:03 +0300

    test writed

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,34 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe "users/index", type: :system do
+RSpec.describe 'users #index', type: :feature do
   before :each do
-    @user = User.create(
+    user1 = User.create(
       name: 'Gabriel',
       photo: 'https://www.pexels.com/photo/silhouette-of-a-person-on-a-swing-3293148/',
       bio: "I'm a student at Microverse",
       posts_counter: 0
     )
+    user2 = User.create(
+      name: 'Second User',
+      photo: 'https://www.pexels.com/photo/silhouette-of-a-person-on-a-swing-3293148/',
+      bio: "I'm not a student at Microverse",
+      posts_counter: 0
+    )
+    @users = [user1, user2]
+    visit '/users'
   end
-  describe 'index page' do
-    it 'displays all users' do
-      visit users_path
-      expect(page).to have_content(@user.name)
-    end
 
-    it 'displays all users pictures' do
-      visit users_path
-      expect(page).to have_css("img[src*='#{@user.photo}']")
+  it 'displays all users cards' do
+    @users.each do |user|
+      expect(page).to have_content(user.name)
+      
     end
+  end
 
-    it 'shows the user number of posts' do
-      visit users_path
-      expect(page).to have_content(@user.posts_counter)
-    end
-
-    it 'redirects to user page' do
-      visit users_path
-      click_link('Gabriel')
-      expect(page).to have_content(@user.name)
-    end
+  it 'changes to user profile after clicking on a card' do
+    click_link(@users.first.name.to_s)
+    expect(page).to have_content('Profile')
   end
 end
--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'user_path', type: :system do
+RSpec.describe 'users #show', type: :feature do
   before :each do
     @user = User.create(
       name: 'Gabriel',
@@ -13,51 +13,42 @@ RSpec.describe 'user_path', type: :system do
     Post.create(user: @user, title: 'Title3', text: 'Text3', comments_counter: 0, likes_counter: 0)
     Post.create(user: @user, title: 'Title4', text: 'Text4', comments_counter: 0, likes_counter: 0)
     Post.create(user: @user, title: 'Title5', text: 'Text5', comments_counter: 0, likes_counter: 0)
-    Post.create(user: @user, title: 'Title6', text: 'Text6', comments_counter: 0, likes_counter: 0)
+    User.create(
+      name: '1st',
+      photo: 'https://www.pexels.com/photo/silhouette-of-a-person-on-a-swing-3293148/',
+      bio: "I'm a student at Microverse",
+      id: 1,
+      posts_counter: 0
+    )
+    visit "/users/#{@user.id}"
   end
 
-  describe 'user show page' do
-    it 'shows the user\'s photo' do
-      visit user_path(@user)
-      expect(page).to have_css("img[src*='#{@user.photo}']")
-    end
+  it 'displays the user profile' do
+    expect(page).to have_content(@user.name)
+    expect(page).to have_css("img[src*='#{@user.photo}']")
+    expect(page).to have_content("#{@user.posts_counter} posts in total.")
+    expect(page).to have_content(@user.bio)
+  end
 
-    it 'shows the user\'s username' do
-      visit user_path(@user)
-      expect(page).to have_content(@user.name)
+  it 'displays the users first three posts' do
+    recent_posts = @user.recent_posts
+    recent_posts.each do |post|
+      expect(page).to have_content(post.title)
+      expect(page).to have_content(post.text)
     end
+  end
 
-    it 'shows the user\'s bio' do
-      visit user_path(@user)
-      expect(page).to have_content(@user.bio)
-    end
+  it 'contains a button that shows all posts' do
+    expect(page).to have_content('See all posts')
+  end
 
-    it 'shows the user\'s number of posts' do
-      visit user_path(@user)
-      expect(page).to have_content(@user.posts_counter)
-    end
+  it 'redirects the user to the complete posts list' do
+    click_link('See all posts')
+    expect(current_path).to eq("/users/#{@user.id}/posts")
+  end
 
-    it 'shows the user\'s first 3 posts' do
-      visit user_path(@user)
-      @user.recent_posts.each do |post|
-        expect(page).to have_content(post.title)
-      end
-    end
-
-    it 'shows the user\'s button to view all posts' do
-      visit user_path(@user)
-      expect(page).to have_button('See all posts')
-    end
-
-    it 'redirects to that user\'s post when clicked' do
-      visit user_path(@user)
-      click_link("#{@user.posts.last.title}")
-      expect(page).to have_current_path(user_post_path(@user.id, @user.posts.last.id))
-    end
-
-    it 'redirects to the user\'s post\'s index page' do
-      visit user_path(@user)
-      expect(page).to have_link('See all posts')
-    end
+  it 'redirects the user to the post show page' do
+    click_link(@user.posts.last.title.to_s)
+    expect(current_path).to eq("/users/#{@user.id}/posts/#{@user.posts.last.id}")
   end
 end
