dce3db4ad2ae53b135d8dc5c490c08d1c0f6b217
Gabriel <gopxfs@hotmail.com>
2022-09-28 11:53:14 -0300

    Add validation to allow only one like per user (for each post)

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  validates :user, uniqueness: { scope: :post }
+
   def update_likes_counter
     post.update(likes_counter: post.likes.count)
   end
