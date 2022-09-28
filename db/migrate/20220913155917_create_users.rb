7fd6b88af6ffa56c8e031533a64daf0b3cf5795d
masangana <alexmasangana@gmail.com>
2022-09-28 13:50:29 +0300

    Update

--- db/migrate/20220913155917_create_users.rb
+++ db/migrate/20220913155917_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :photo, default: 'https://cdn-icons-png.flaticon.com/512/1159/1159797.png?'
-      t.text :bio, default: 'This user has no bio yet'
+      t.string :photo, default: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png?'
+      t.text :bio, default: 'This user has no bio yet but will soon!'
       t.integer :posts_counter, default: 0
 
       t.timestamps
